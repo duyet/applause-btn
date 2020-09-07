cd360d1fcc6846ddd5186535d210454d2a291f80
Duyet Le <lvduyet@fossil.com>
2020-09-07 23:36:31 +0700

    Fix CORS

--- server.go
+++ server.go
@@ -11,7 +11,6 @@ import (
 
 func main() {
 	app := Setup()
-	app = SetupMiddleware(app)
 
 	// Start server
 	log.Fatal(app.Listen(3000))
@@ -21,27 +20,21 @@ func main() {
 func Setup() *fiber.App {
 	app := fiber.New()
 
-	// Setup routes
-	app.Get("/", func(c *fiber.Ctx) {
-		c.Send("Hello, World!")
-	})
-
-	app.Get("/get-claps", api.GetClaps)
-	app.Post("/get-multiple", api.GetMultiple)
-	app.Post("/update-claps", api.UpdateClaps)
-	app.Static("/public", "./public")
-
-	return app
-}
-
-// SetupMiddleware setup middleware for app
-func SetupMiddleware(app *fiber.App) *fiber.App {
 	// CORS
 	app.Use(cors.New())
 	// Compression config
 	app.Use(middleware.Compress())
 	// Logger
 	app.Use(middleware.Logger())
+
+	// Setup routes
+	app.Get("/", func(c *fiber.Ctx) {
+		c.Send("Hello, World!")
+	})
+	app.Get("/get-claps", api.GetClaps)
+	app.Post("/get-multiple", api.GetMultiple)
+	app.Post("/update-claps", api.UpdateClaps)
+	app.Static("/public", "./public")
 
 	return app
 }
